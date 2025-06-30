48c06ca644d0242e612b362a84f8ec003a285371
이정화 <leejeongwha@nhn.com>
2025-06-30 16:22:30 +0900

    방문자 패턴 수정

--- src/main/java/org/example/moderndesign/visitor/Fruit.java
+++ src/main/java/org/example/moderndesign/visitor/Fruit.java
@@ -10,6 +10,13 @@ public class Fruit implements ItemElement {
     private int weight;
     private String name;
 
+    /**
+     * 더블 디스패치(double dispatch)
+     * 메서드 호출 시 객체의 타입(자신)과 파라미터의 타입(인자) 두가지 모두에 따라 실행될 메서드가 결정 되는 메커니즘
+     * 오버로딩(정적 바인딩)과 오버라이딩(동적 바인딩)을 모두 사용하여 적절하게 더블 디스패치 구현
+     * instanceof 없이 타입별 분기 처리 가능
+     * 
+     */
     @Override
     public int apply(ShoppingCartVisitor visitor) {
         return visitor.visit(this);
